110503db4a4accde9283873ea1ff6e353b34347c
solankiRahulFr <solankirahul.fr@gmail.com>
2024-01-26 16:50:34 +0100

    base path

--- svelte.config.js
+++ svelte.config.js
@@ -14,7 +14,7 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			base: 'deep_components'
 		}
 	}
 };
