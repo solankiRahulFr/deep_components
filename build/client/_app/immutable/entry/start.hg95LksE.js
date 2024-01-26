d791d78c4c53365f6c81ad3993aee6b84dd8374d
solankiRahulFr <solankirahul.fr@gmail.com>
2024-01-26 17:12:20 +0100

    changing to vercel

--- build/client/_app/immutable/entry/start.hg95LksE.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.Qa6qL3zS.js";export{t as start};
--- build/server/chunks/1-q3YMu0j8.js
+++
@@ -1,9 +0,0 @@
-const index = 1;
-let component_cache;
-const component = async () => component_cache ??= (await import('./error.svelte-JUWCfP2n.js')).default;
-const imports = ["_app/immutable/nodes/1.CEBmkEKw.js","_app/immutable/chunks/scheduler.RvOW76O7.js","_app/immutable/chunks/index.3B0PiDA_.js","_app/immutable/chunks/stores.iLyG2DE-.js","_app/immutable/chunks/entry.Qa6qL3zS.js","_app/immutable/chunks/index.Arc6vgbi.js"];
-const stylesheets = [];
-const fonts = [];
-
-export { component, fonts, imports, index, stylesheets };
-//# sourceMappingURL=1-q3YMu0j8.js.map
